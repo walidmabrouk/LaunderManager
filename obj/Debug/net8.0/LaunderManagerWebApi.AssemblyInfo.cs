7a945018cb81da4f10fb363457a9e348661b62da
Walid MABROUK <walid.mabrouk@edu.isetcom.tn>
2025-01-08 19:02:11 +0100

    Accomplished WebAPI Server

--- obj/Debug/net8.0/LaunderManagerWebApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/LaunderManagerWebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LaunderManagerWebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73b49e9d5bbfc8efeabf930f7d2ffe6049f44542")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+885835c27093fc28a6169e575996edba127cca37")]
 [assembly: System.Reflection.AssemblyProductAttribute("LaunderManagerWebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LaunderManagerWebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
